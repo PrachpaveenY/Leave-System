d14eddee025b1d3f8886df5c881e2abdaf6677aa
Prachpaveen Y <prachpaveen888@gmail.com>
2023-02-06 23:14:57 +0700

    first

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <!-- list -->
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
