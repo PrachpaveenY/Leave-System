e3d90e95bab345fb74b5c30f4793fb6890de72cd
Prachpaveen Y <prachpaveen888@gmail.com>
2023-02-07 15:51:48 +0700

    mongodb go

---
+++ src/router/index.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	fmt.Println("Welcome to Server")
+
+	e := echo.New()
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "postgres" || password == "2565" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	log.Fatal(e.Start(":2565"))
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 8*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
+
+// close connection
+var db *sql.DB
+
+// Get database url from environment variable, Create Table
+func InitDB() {
+	var err error
+	// connect to MongoDB
+	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS <name> (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount FLOAT,
+		note TEXT,
+		tags TEXT[] );
+	`
+	_, err = db.Exec(createTb)
+
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+
+}
+
+// ===========
+
+// connect to MongoDB
+// db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+// if err != nil {
+// 	fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+// 	os.Exit(1)
+// }
+// defer db.Close()
+
+// 	var greeting string
+// 	err = db.QueryRow("select 'Hello'").Scan(&greeting)
+// 	if err != nil {
+// 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+// 		os.Exit(1)
+// 	}
+
+// 	fmt.Println(greeting)
